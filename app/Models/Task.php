d3f306277a83b6b1a1c064b4fe88fb48185b1538
Pratyay <0909passingh@gmail.com>
2026-08-14 20:26:31 +0530

    first commit for backend

---
+++ app/Models/Task.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Task extends Model
+{
+    protected $fillable = [
+        'project_id',
+        'title',
+        'description',
+        'status',
+        'due_date'
+    ];
+
+    public function Project(){
+        return $this->belongsTo(Project::class);
+    }
+}
